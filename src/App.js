f89edb5c11bee3881b4974cd74ab21a5d4a66019
Ameen <ameenaladdin@gmail.com>
2023-08-09 21:01:07 -0400

    HTTP Test

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
 
     // Send a request to the Flask API
     const response = await fetch(
-      `https://mc-master-seat-alert.vercel.app/open_seats?course_code=${courseCode}&term=${term}`
+      `http://nginxseatalert.duckdns.org/open_seats?course_code=${courseCode}&term=${term}`
     );
     const json = await response.json();
 
@@ -85,7 +85,7 @@ function App() {
   const handleButtonClick = async () => {
     if (!error) {
       const response = await fetch(
-        `https://mc-master-seat-alert.vercel.app/notify_open_seats?course_code=${courseCode}&term=${term}&section=${selectedSection}&contact_method=${contactMethod}&contact_info=${contactInfo}`
+        `http://nginxseatalert.duckdns.org/notify_open_seats?course_code=${courseCode}&term=${term}&section=${selectedSection}&contact_method=${contactMethod}&contact_info=${contactInfo}`
       );
       if (response.status === 200) {
         setOpen(true);
