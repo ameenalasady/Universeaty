86ceb1eec7072973970fa26c537af0162b5d30c6
Ameen <ameenaladdin@gmail.com>
2024-06-29 23:10:59 -0400

    Fix h error

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GetStarted = () => {
   return (
     <div className="getstartedcontainer">
-      <h className="getstarted">Get Started</h>
+      <div className="getstarted">Get Started</div>
       <img
         className="startedarrow"
         src="/arrow-sm-right-svgrepo-com.svg"
